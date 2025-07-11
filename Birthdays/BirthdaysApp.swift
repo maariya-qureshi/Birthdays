72e98f35309b1725192518dd02337ca71bfbf6f0
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-11 07:29:35 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Maariya Qureshi on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
